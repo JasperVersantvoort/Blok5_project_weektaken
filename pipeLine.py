31c577647b846577f1a48dfe0d2c581564b586a1
christiaan posthuma <christiaanposthuma@gmail.com>
2020-10-02 13:47:00 +0200

    push again...

--- pipeLine.py
+++ pipeLine.py
@@ -9,7 +9,8 @@ class pipeline():
 
     def __init__(self, input_file_name):
         print("created new instance of pipeline")
-        self.input_file = input_file_name
+        self.fasta_to_msa = input_file_name
+
 
     def clustalO(self,output_file):
         """Runs ClustalO on linux command line
@@ -27,16 +28,36 @@ class pipeline():
             pass
         # If not, command will run on the command line
         else:
-            cmd = "clustalo -i {} -o {}".format(self.input_file, output_file)
+            cmd = "clustalo -i {} -o {}".format(self.fasta_to_msa, output_file)
+            e = subprocess.check_call(cmd, shell=True)
+        return
+
+    def mafft(self,output_file):
+        """Runs ClustalO on linux command line
+
+        Input:
+        input_file - str, name of fasta file to create MSA
+        output_file - str,name output of ClustalO
+
+        Return:
+        ClustalO returns a file, the file can be found in the current path
+        unless otherwise specified
+        """
+        # Checks id file exists
+        if os.path.isfile(output_file):
+            pass
+        # If not, command will run on the command line
+        else:
+            cmd = "mafft --auto {} > {}".format(self.fasta_to_msa, output_file)
             e = subprocess.check_call(cmd, shell=True)
         return
 
 
-    def hmmer(output_bestand):
+    def hmmer(self,input_file,output_file):
         """Runs Hmmr on linux command line
 
         Input:
-        output_file
+        MSA file that
 
         Return:
         hmmr retuns a file with the hmm profile, this van be found in the current path
@@ -56,16 +77,22 @@ class pipeline():
 
 fasta_to_msa  = filedialog.askopenfilename()
 print(fasta_to_msa)
-MSA_to_HMM = "MSA_{}".format("")
-output_HMM = "HMM_{}".format("")
+MSA_to_HMM = "MSA_{}".format("") # name of the file
+output_HMM = "HMM_{}".format("") # name of the file
+
+
+pipeline = pipeline(fasta_to_msa) #create a new object, with the fasta inside
+
+pipeline.clustalO(output_file=MSA_to_HMM) # runs clustalo
+pipeline.mafft(output_file=MSA_to_HMM)
+
+MSA_to_HMM_file = filedialog.askopenfilename()
+
+pipeline.hmmer(input_file=MSA_to_HMM_file,output_file=output_HMM)
+
+
 
 # to run on command line:
 # fasta_to_msa  = argv[1]
 # MSA_to_HMM = = argv[2]
 # output_HMM = = argv[3]
-
-pipeline = pipeline(fasta_to_msa)
-pipeline.clustalO(output_file=MSA_to_HMM)
-pipeline.hmmer(output_HMM)
-
-
